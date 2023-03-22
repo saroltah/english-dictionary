900c50cbde3def2e61dbe0ec584bd0132c53ccf0
Sarolta <sarolta.hegyi@gmail.com>
2023-03-22 21:26:08 +0100

    added axios

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,5 +1,6 @@
 import React, {useState} from "react";
 import "./Dictionary.css"
+import axios from "axios";
 
 export default function (Dictionary){
 let [mainWord, setMainWord]=useState(null)
@@ -7,6 +8,13 @@ function search(event){
     event.preventDefault();
     alert (`Searching for ${mainWord}`);
 }
+
+function handleResponse(response){
+    console.log(response.data[0])
+}
+
+let apiUrl= `https://api.dictionaryapi.dev/api/v2/entries/en/${mainWord}`
+axios.get(apiUrl).then(handleResponse)
 
 function changeWord(event){
     setMainWord(event.target.value)

0f489be4603b9443a5a9f799cbe57c6b96c8b8f0
Sarolta <sarolta.hegyi@gmail.com>
2023-03-23 07:36:55 +0100

    started to get the props

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import React from 'react'
 import Dictionary from "./Dictionary"
 
 export default function (App){
-  return(<div> <Dictionary/>
+  return(<div>
+    <header>
+        <h1>English Dictionary</h1>
+         <Dictionary/>
+         </header>
   <footer>Coded by Sarolta Hegyi</footer></div>)
 }
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,32 +1,32 @@
 import React, {useState} from "react";
 import "./Dictionary.css"
 import axios from "axios";
+import Results from "./Results";
 
-export default function (Dictionary){
+export default function Dictionary(){
 let [mainWord, setMainWord]=useState(null)
+let [results, setResults]=useState(null)
+
 function search(event){
-    event.preventDefault();
-    alert (`Searching for ${mainWord}`);
+    event.preventDefault()
+    let apiUrl= `https://api.dictionaryapi.dev/api/v2/entries/en/${mainWord}`
+axios.get(apiUrl).then(handleResponse);
 }
 
 function handleResponse(response){
-    console.log(response.data[0])
+    console.log(response.data[0].meanings[0])
+    setResults(response.data[0])
 }
-
-let apiUrl= `https://api.dictionaryapi.dev/api/v2/entries/en/${mainWord}`
-axios.get(apiUrl).then(handleResponse)
 
 function changeWord(event){
     setMainWord(event.target.value)
 }
     
     return(<div>
-        <header>
-        <h1>English Dictionary</h1>
         <form onSubmit={search}>
             <input type="search" onChange={changeWord}></input>
             <input type="submit"></input>
         </form>
-        </header>
-    </div>)
+        <Results results={results}/>
+    </div>);
 }

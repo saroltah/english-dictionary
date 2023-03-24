2c11fcef9af5fff39a8aad958c65901c45527f50
Sarolta <sarolta.hegyi@gmail.com>
2023-03-24 13:40:42 +0100

    trying to solve bug

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import React from 'react'
 import Dictionary from "./Dictionary"
 
 export default function (App){
-  return( <div className="App">
+  return(<div className="App">
     <div className="header">
         <h1>English Dictionary</h1>
          </div>
-         <div className="Search"
-            href="https://cool-entremet-69e2c9.netlify.app/"> Sarolta Hegyi</a
+         <div className="Search">
+         <h2>Choose your word:</h2>
+           <Dictionary/>
+           </div>
+  <footer>Coded by <a target="_blank"
+            href="https://cool-entremet-69e2c9.netlify.app/"
+            > Sarolta Hegyi</a
           > and has open source code on <a target="_blank" href="https://github.com/saroltah/english-dictionary">Github</a>. </footer>
-  </div>
-  )
+  </div>)
 }

98064fc6bda623e4195055d32c212f8bf4fcd943
Sarolta <sarolta.hegyi@gmail.com>
2023-03-26 16:35:36 +0200

    refreshed everything

--- src/Hero.js
+++ src/Hero.js
@@ -3,10 +3,8 @@ import "./Hero.css";
 
 export default function Hero(){
     return(
-        <div className="hero">
-             <header>
+        <div className="Hero">
         <h1>English Dictionary</h1>
-      </header>
         </div>
     )
 }

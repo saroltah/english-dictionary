e0ee788d35129e3d182f885a3a18ca966e43d6f5
Sarolta <sarolta.hegyi@gmail.com>
2023-03-26 15:56:19 +0200

    updated app.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,37 @@
-import './App.css'
-import React from 'react'
-import Dictionary from "./Dictionary"
+import "./App.css";
+import React from "react";
+import Dictionary from "./Dictionary";
 
-export default function (App){
-  return(
-  <div className="App">
-    <header>
+export default function App() {
+  return (
+    <div className="App">
+      <header>
         <h1>English Dictionary</h1>
-        </header>
-         <div className="Search">
-         <h2>Choose your word:</h2>
-           <Dictionary/>
-           </div>
-  <footer>Coded by<a target="_blank"
-            href="https://cool-entremet-69e2c9.netlify.app/"
-            rel="noreferrer"
-            >{" "}Sarolta Hegyi</a
-          >{" "}and has open source code on{" "}<a target="_blank" href="https://github.com/saroltah/english-dictionary"  rel="noreferrer">Github</a>.</footer>
-  </div>)
+      </header>
+      <div className="Search">
+        <h2>Choose your word:</h2>
+        <Dictionary />
+      </div>
+      <footer>
+        Coded by{" "}
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://cool-entremet-69e2c9.netlify.app/"
+        >
+          {" "}
+          Sarolta Hegyi
+        </a>{" "}
+        and has open source code on{" "}
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://github.com/saroltah/english-dictionary"
+        >
+          Github
+        </a>
+        .{" "}
+      </footer>
+    </div>
+  );
 }

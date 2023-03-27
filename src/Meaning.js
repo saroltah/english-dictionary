72d83e166ee29dee93aa1afa2e09acd1793357a7
Sarolta <sarolta.hegyi@gmail.com>
2023-03-27 18:03:33 +0200

    added synonyms

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,5 +1,6 @@
 import React from "react"
 import "./Meaning.css"
+import Synonyms from "./Synonyms"
 
 export default function Meaning(props){
     console.log(props.meaning);
@@ -12,7 +13,9 @@ export default function Meaning(props){
             <p>
                 {definition.definition}
                 <br/>
+                
                <em>{definition.example}</em> 
+               <Synonyms synonyms={definition.synonyms}/>
                 </p>
    </div>
         );

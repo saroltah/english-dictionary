405b4cc126dc7c0f898b4b649e5ec3012a9207fd
Sarolta <sarolta.hegyi@gmail.com>
2023-03-27 18:34:27 +0200

    added audio

---
+++ src/Phonetics.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./Phonetics.css"
+
+export default function Phonetics(props){
+   return  (<div className="Phonetics">
+    <a href={props.phonetic.audio} target="_blank"> Listen </a>
+    <br/>
+    {props.phonetic.text}
+   </div>)}
--- src/Results.js
+++ src/Results.js
@@ -1,6 +1,7 @@
 import "./Results.css"
 import React from "react"
 import Meaning from "./Meaning.js"
+import Phonetics from "./Phonetics"
 
 export default function Results(props){
 if (props.results)
@@ -8,6 +9,13 @@ if (props.results)
     return  <div className="Results">
         <h2>{props.results.word}</h2>
 
+        {props.results.phonetics.map(function(phonetic, index)
+        {
+            return(<div key={index}>
+        <Phonetics phonetic={phonetic}/>
+</div>
+            );
+        })}
         {props.results.meanings.map(function(meaning, index){
 return (<div key={index}>
     <Meaning meaning={meaning}/>

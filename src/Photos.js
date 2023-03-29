dfd023e2c0d5d549166be6eff6f68a85df44a0d6
Sarolta <sarolta.hegyi@gmail.com>
2023-03-29 19:23:44 +0200

    changed to show only 1 audio

--- src/Photos.js
+++ src/Photos.js
@@ -2,7 +2,6 @@ import "./Photos.css"
 import React from "react"
 
 export default function Photos(props){
-    console.log(props.photos)
     if(props.photos){
     return <div className="Photos">
         {props.photos.map(function(photo, index) {
--- src/Results.js
+++ src/Results.js
@@ -4,18 +4,17 @@ import Meaning from "./Meaning.js"
 import Phonetics from "./Phonetics"
 
 export default function Results(props){
+    console.log(props.results)
 if (props.results)
 {
     return  <div className="Results">
+          
         <h2>{props.results.word}</h2>
 
-        {props.results.phonetics.map(function(phonetic, index)
-        {
-            return(<div key={index}>
-        <Phonetics phonetic={phonetic}/>
+        <div>
+        <Phonetics phonetic={props.results.phonetics[0]}/>
 </div>
-            );
-        })}
+       
         {props.results.meanings.map(function(meaning, index){
 return (<div key={index}>
     <section>
